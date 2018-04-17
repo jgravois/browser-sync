3f53d5fdd373488669085f9b66f541603df066e0
Shane Osbourne <shane.osbourne8@gmail.com>
2018-04-17 17:22:14 +0100

    set/unset click handlers on the fly

--- client/lib/listeners/clicks.listener.ts
+++ client/lib/listeners/clicks.listener.ts
@@ -6,49 +6,47 @@ import * as ClickEvent from "../messages/ClickEvent";
 import { withLatestFrom } from "rxjs/operators/withLatestFrom";
 import { filter } from "rxjs/operators/filter";
 import { map } from "rxjs/operators/map";
-import { share } from "rxjs/operators/share";
 import {Inputs} from "../index";
-import {mergeMap} from "rxjs/operators/mergeMap";
 import {pluck} from "rxjs/operators/pluck";
+import {skip} from "rxjs/operators/skip";
+import {distinctUntilChanged} from "rxjs/operators/distinctUntilChanged";
+import {switchMap} from "rxjs/operators/switchMap";
+import {fromEvent} from "rxjs/observable/fromEvent";
+import {empty} from "rxjs/observable/empty";
 
 export function getClickStream(
     document: Document,
-    socket$: Inputs['socket$']
-    , option$: Inputs['option$']): Observable<OutgoingSocketEvent> {
+    socket$: Inputs['socket$'],
+    option$: Inputs['option$']): Observable<OutgoingSocketEvent> {
     const canSync$ = createTimedBooleanSwitch(
         socket$.pipe(filter(([name]) => name === IncomingSocketNames.Click))
     );
 
     return option$.pipe(
+        skip(1), // initial option set before the connection event
         pluck('ghostMode', 'clicks'),
-        filter(clicks => clicks === true),
-        mergeMap(() => {
-            return clickObservable(document).pipe(
+        distinctUntilChanged(),
+        switchMap((canClick) => {
+            if (!canClick) {
+                return empty();
+            }
+            return fromEvent(document, 'click', true).pipe(
+                map((e: Event) => e.target),
+                filter((target: any) => {
+                    if (target.tagName === "LABEL") {
+                        const id = target.getAttribute("for");
+                        if (id && document.getElementById(id)) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }),
                 withLatestFrom(canSync$),
                 filter(([, canSync]) => canSync),
-                map((incoming): OutgoingSocketEvent => {
-                    const clickEvent: { target: HTMLElement } = incoming[0];
-                    return ClickEvent.outgoing(getElementData(clickEvent.target));
+                map(([target]): OutgoingSocketEvent => {
+                    return ClickEvent.outgoing(getElementData(target));
                 })
             );
         })
     )
-}
-
-function clickObservable(document: Document) {
-    return Observable.create(obs => {
-        document.body.addEventListener(
-            "click",
-            function(e: any) {
-                if (e.target.tagName === "LABEL") {
-                    const id = e.target.getAttribute("for");
-                    if (id && document.getElementById(id)) {
-                        return false;
-                    }
-                }
-                obs.next({ target: e.target });
-            },
-            true
-        );
-    }).pipe(share());
 }
